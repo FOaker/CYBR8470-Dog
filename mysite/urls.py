3b8f265719282cf16dff263bfda76acede0f9d7c
Dong <70738721+FOaker@users.noreply.github.com>
2021-10-20 20:38:50 -0500

    update root URL

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    url(r'^', include('dogs.urls')),
+    url(r'^admin/', admin.site.urls),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    url(r'^api/', include('dogs.urls')),
 ]
